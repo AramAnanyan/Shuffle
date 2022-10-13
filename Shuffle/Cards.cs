0503bc753bf58d5e7b395cf7b3c6218e39f8bc15
AramAnanyan <aramo.ananyan@gmail.com>
2022-10-13 16:39:56 +0400

    Add project files.

---
+++ Shuffle/Cards.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shuffle
+{
+    public static class Cards
+    {
+        public static void ShuffleCards(ref int[] arr)
+        {
+            Random random = new Random();   
+            for(int i = 0; i<arr.Length-1; i++)
+            {
+                int k = random.Next(i,arr.Length);
+                int val=arr[k];
+                arr[k] = arr[i];
+                arr[i] = val;
+            }
+        }
+    }
+}
---
+++ Shuffle/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Shuffle;
+
+int[] arr = new int[52];
+for(int i = 0; i < arr.Length; i++)
+{
+    arr[i] = i+1;
+}
+
+
+Cards.ShuffleCards(ref arr);
+
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.WriteLine(arr[i]);
+}
